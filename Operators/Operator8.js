740baa9c40f87dc78b197cc61027e560081a58ff
khushboo kumari <ksingh200601@gmail.com>
2025-02-05 01:29:38 +0530

    operators solution modified

--- Operators/Operator8.js
+++ Operators/Operator8.js
@@ -5,6 +5,7 @@
 function calculateTimeDifference(time1, time2) {
 // see this after
     const [hours1, minutes1] = time1.split(':').map(Number);
+    
     const [hours2, minutes2] = time2.split(':').map(Number);
     const totalMinutes1 = hours1 * 60 + minutes1;
     const totalMinutes2 = hours2 * 60 + minutes2;
